4e8fd6551fe658cb98bb54e3781b8382c606780f
Louis Blonde <louis.blonde24@gmail.com>
2025-06-08 01:30:44 +0200

    add autocomplete

--- app.py
+++ app.py
@@ -9,6 +9,17 @@ def lb_get_key(key):
     except:
         if key == keyboard.Key.cmd and not open:
             shortcut = True
+def lb_auto_complete(search):
+    possible_search = []
+    for i in pre_key:
+        if i[:len(search)] == search:
+            possible_search.append(i)
+    if len(possible_search) != 0 and search != "":
+        label_autocomplete.configure(text=possible_search[0][len(search):])
+        label_autocomplete.place(x=(157 + 8.8 * len(search)), y=2.5)
+    else:
+        label_autocomplete.configure(text="")
+    print(possible_search)
 
 class App(ctk.CTk):
     def __init__(self, title, dimension):
@@ -27,8 +38,11 @@ root = App("Raycast", "800x300")
 to_do_request = ctk.StringVar()
 to_do = ctk.CTkEntry(root, width=500, textvariable=to_do_request, placeholder_text="Hello", fg_color="#1A1A2E", border_color="#1723CA", text_color="white", font=("Monospace", 18))
 to_do.pack()
+to_do.bind("<KeyRelease>", lambda key: lb_auto_complete(to_do_request.get()))
 console = ctk.CTkScrollableFrame(root, border_width=2, width=720, height=200, fg_color="#1A1A2E", border_color="#1723CA", scrollbar_button_color="#1723CA", scrollbar_button_hover_color="#2A34D4")
 console.pack()
+label_autocomplete = ctk.CTkLabel(root, height=20, text="", text_color="#5A5A5A", fg_color="#1A1A2E", font=("Monospace", 16))
+label_autocomplete.place(x=(157 + 10 * len(to_do_request.get())), y=2)
 label = ctk.CTkLabel(console, text="SSSSSsss", anchor="w")
 label.pack(fill="x")
 
