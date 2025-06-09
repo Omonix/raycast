33f9b199a74220783c186fa08e6a63202da647b5
Louis Blonde <louis.blonde24@gmail.com>
2025-06-09 15:16:02 +0200

    add history console

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from pynput import keyboard
+from PIL import Image
 import cohere, json, webbrowser
 
 with open("./env_var.json") as env:
@@ -22,14 +23,15 @@ def lb_autocomplete(search, key):
     if len(possible_search) != 0 and search != "":
         label_autocomplete.configure(text=possible_search[0]["name"][len(search):])
         label_autocomplete.place(x=(130 + 8.8 * len(search)), y=2.5)
-    else:
+    elif search != "":
         label_autocomplete.configure(text="")
+        label_autocomplete.place(x=1000, y=1000)
+    else:
+        label_autocomplete.configure(text="Enter a command")
+        label_autocomplete.place(x=130, y=2.5)
     if key.char == "\r" and len(possible_search) != 0:
         to_do_request.set(possible_search[0]["name"] + " ")
-        to_do.configure(fg_color=colors[possible_search[0]["index_color"]]["fg_color"], border_color=colors[possible_search[0]["index_color"]]["border_color"], text_color=colors[possible_search[0]["index_color"]]["color_text_input"])
-        label_autocomplete.configure(text="", fg_color=colors[possible_search[0]["index_color"]]["fg_color"])
-        console_box.configure(fg_color=colors[possible_search[0]["index_color"]]["fg_color"], border_color=colors[possible_search[0]["index_color"]]["border_color"], scrollbar_button_color=colors[possible_search[0]["index_color"]]["border_color"], scrollbar_button_hover_color=colors[possible_search[0]["index_color"]]["border_color"])
-        tabs.configure(text_color=colors[possible_search[0]["index_color"]]["color_text_input"], segmented_button_fg_color=colors[possible_search[0]["index_color"]]["fg_color"], segmented_button_selected_color=colors[possible_search[0]["index_color"]]["border_color"], segmented_button_selected_hover_color=colors[possible_search[0]["index_color"]]["border_color"], segmented_button_unselected_color=colors[possible_search[0]["index_color"]]["fg_color"], segmented_button_unselected_hover_color=colors[possible_search[0]["index_color"]]["fg_color"])
+        lb_reset_colors(possible_search)
         to_do.icursor("end")
     elif key.char == '\r':
         request = to_do_request.get().strip().split(" ")
@@ -37,18 +39,16 @@ def lb_autocomplete(search, key):
         for i in pre_key:
             if i["name"] == command:
                 if i["type"] == "website":
-                    separator = "+"
-                    request = separator.join(request)
-                    webbrowser.open(f"https://www.{i["adress"]}/{i["search_query"] + request}")
+                    lb_to_website(request, i)
                 elif i["type"] == "ia":
-                    separator = " "
-                    request = separator.join(request)
                     lb_cohere_request(request)
         for i in console_element_list:
             i.pack_forget()
             i.pack(fill="x", pady=10)
         to_do_request.set(command + " ")
 def lb_cohere_request(request):
+    separator = " "
+    request = separator.join(request)
     console_response = ctk.CTkTextbox(console_box, fg_color="#2E2E2E", text_color=colors[pre_key[0]["index_color"]]["color_text_input"], font=("Monospace", 18), corner_radius=20)
     console_element_list.insert(0, console_response)
     try:
@@ -57,6 +57,42 @@ def lb_cohere_request(request):
     except:
         console_response.insert("0.0", "ERROR")
     console_response.configure(state="disabled")
+def lb_to_website(request, e):
+    try:
+        image = Image.open(e["icon"])
+    except:
+        image = Image.open("./assets/img/iconDefault.jpg")
+    image_comp = ctk.CTkImage(light_image=image, dark_image=image, size=(48, 48))
+    console_response = ctk.CTkLabel(console_box, image=image_comp, compound="left", text=f"  Search {" ".join(request)} on {e["name"]} ({e["adress"]})", fg_color="#2E2E2E", text_color="#ffffff", font=("Monospace", 18), corner_radius=20)
+    console_element_list.insert(0, console_response)
+    separator = "+"
+    request = separator.join(request)
+    webbrowser.open(f"https://www.{e["adress"]}/{e["search_query"] + request}")
+def lb_reset_colors(possible_search):
+    to_do.configure(fg_color=colors[possible_search[0]["index_color"]]["fg_color"], border_color=colors[possible_search[0]["index_color"]]["border_color"], text_color=colors[possible_search[0]["index_color"]]["color_text_input"])
+    label_autocomplete.configure(text="", fg_color=colors[possible_search[0]["index_color"]]["fg_color"])
+    console_box.configure(fg_color=colors[possible_search[0]["index_color"]]["fg_color"], border_color=colors[possible_search[0]["index_color"]]["border_color"], scrollbar_button_color=colors[possible_search[0]["index_color"]]["border_color"], scrollbar_button_hover_color=colors[possible_search[0]["index_color"]]["border_color"])
+    tabs.configure(text_color=colors[possible_search[0]["index_color"]]["color_text_input"], segmented_button_fg_color=colors[possible_search[0]["index_color"]]["fg_color"], segmented_button_selected_color=colors[possible_search[0]["index_color"]]["border_color"], segmented_button_selected_hover_color=colors[possible_search[0]["index_color"]]["border_color"], segmented_button_unselected_color=colors[possible_search[0]["index_color"]]["fg_color"], segmented_button_unselected_hover_color=colors[possible_search[0]["index_color"]]["fg_color"])
+def lb_handle_type_action(value):
+    button_add_action.pack_forget()
+    if value == "Website":
+        adress_add_action.pack()
+        query_add_action.pack()
+        adress_label.place(x=75, y=117)
+        query_label.place(x=75, y=145)
+    else:
+        adress_add_action.pack_forget()
+        query_add_action.pack_forget()
+        adress_label.place(x=1000, y=1000)
+        query_label.place(x=1000, y=1000)
+    button_add_action.pack()
+def lb_show_placeholder(value, text, label, x, y=32):
+    if value.get() == "":
+        label.configure(text=text, font=("Monospace", 15))
+        label.place(x=x, y=y)
+    else:
+        label.configure(text="", font=("Monospace", 1))
+        label.place(x=1000, y=1000)
 class App(ctk.CTk):
     def __init__(self, title, dimension):
         super().__init__()
@@ -67,32 +103,59 @@ class App(ctk.CTk):
 
 open = False
 shortcut = False
-pre_key = [{"name": "cohere", "type": "ia", "index_color": 2, "adress": "cohere.com", "search_query": None}, {"name": "chrome", "type": "website", "index_color": 9, "adress": "google.com", "search_query": "search?q="}, {"name": "youtube", "type": "website", "index_color": 0, "adress": "youtube.com", "search_query": "results?search_query="}]
-colors = [{"fg_color": "#2E1A1A", "border_color": "#C91616", "color_text_input": "#ffffff"}, {"fb_color": "#2E221A", "border_color": "#C95516", "color_text_input": "#ffffff"}, {"fg_color": "#2E2B1A", "border_color": "#C9AF16", "color_text_input": "#ffffff"}, {"fg_color": "#1B2E1A", "border_color": "#1FC916", "color_text_input": "#ffffff"}, {"fg_color": "#1A2E2C", "border_color": "#16C9BB", "color_text_input": "#ffffff"}, {"fg_color": "#1C1A2E", "border_color": "#2516C9", "color_text_input": "#ffffff"}, {"fg_color": "#2A1A2E", "border_color": "#7316C9", "color_text_input": "#ffffff"}, {"fg_color": "#2E1A29", "border_color": "#C916A3", "color_text_input": "#ffffff"}, {"fg_color": "#2E1A1F", "border_color": "#C9164C", "color_text_input": "#ffffff"}, {"fg_color": "#BEBEBE", "border_color": "#ffffff", "color_text_input": "#000000"}]
+pre_key = [{"name": "cohere", "type": "ia", "index_color": 2, "adress": "cohere.com", "search_query": None, "icon": ""}, {"name": "chrome", "type": "website", "index_color": 9, "adress": "google.com", "search_query": "search?q=", "icon": ""}, {"name": "youtube", "type": "website", "index_color": 0, "adress": "youtube.com", "search_query": "results?search_query=", "icon": "./assets/img/iconYtb.png"}]
+colors = [{"fg_color": "#2E1A1A", "border_color": "#C91616", "color_text_input": "#ffffff"}, {"fb_color": "#2E221A", "border_color": "#C95516", "color_text_input": "#ffffff"}, {"fg_color": "#2E2B1A", "border_color": "#C9AF16", "color_text_input": "#ffffff"}, {"fg_color": "#1B2E1A", "border_color": "#1FC916", "color_text_input": "#ffffff"}, {"fg_color": "#1A2E2C", "border_color": "#16C9BB", "color_text_input": "#ffffff"}, {"fg_color": "#1C1A2E", "border_color": "#2516C9", "color_text_input": "#ffffff"}, {"fg_color": "#2A1A2E", "border_color": "#7316C9", "color_text_input": "#ffffff"}, {"fg_color": "#2E1A29", "border_color": "#C916A3", "color_text_input": "#ffffff"}, {"fg_color": "#2E1A1F", "border_color": "#C9164C", "color_text_input": "#ffffff"}, {"fg_color": "#BEBEBE", "border_color": "#ffffff", "color_text_input": "#000000"}, {"fg_color": "#", "border_color": "#000000", "color_text_input": "#ffffff"}]
 possible_search = []
 console_element_list = []
+option_element = []
+entry_list = []
 
 root = App("Raycast", "800x340")
-tabs = ctk.CTkTabview(root, width=350, height=250, text_color="#ffffff", segmented_button_fg_color="#1A1A2E", segmented_button_selected_color="#1723CA", segmented_button_selected_hover_color="#2A34D4", segmented_button_unselected_color="#1A1A2E", segmented_button_unselected_hover_color="#2A34D4")
+tabs = ctk.CTkTabview(root, width=350, height=250, text_color="#ffffff", segmented_button_fg_color="#000000", segmented_button_selected_color="#3C3C3C", segmented_button_selected_hover_color="#3C3C3C", segmented_button_unselected_color="#000000", segmented_button_unselected_hover_color="#000000")
 tabs.add("Console")
 tabs.add("Options")
 tabs.pack()
 
 to_do_request = ctk.StringVar()
+name_new_action = ctk.StringVar()
+icon_new_action = ctk.StringVar()
+adress_new_action = ctk.StringVar()
+query_new_action = ctk.StringVar()
 
-to_do = ctk.CTkEntry(tabs.tab("Console"), width=500, textvariable=to_do_request, placeholder_text="Hello", fg_color="#1A1A2E", border_color="#1723CA", text_color="white", font=("Monospace", 18))
+to_do = ctk.CTkEntry(tabs.tab("Console"), width=500, textvariable=to_do_request, placeholder_text="Enter a command", fg_color="#000000", border_color="#3C3C3C", text_color="white", font=("Monospace", 18))
 to_do.pack()
 to_do.bind("")
 to_do.bind("<KeyRelease>", lambda key: lb_autocomplete(to_do_request.get(), key))
-label_autocomplete = ctk.CTkLabel(tabs.tab("Console"), height=20, text="", text_color="#5A5A5A", fg_color="#1A1A2E", font=("Monospace", 16))
-label_autocomplete.place(x=0, y=0)
-console_box = ctk.CTkScrollableFrame(tabs.tab("Console"), border_width=2, width=720, height=200, fg_color="#1A1A2E", border_color="#1723CA", scrollbar_button_color="#1723CA", scrollbar_button_hover_color="#1723CA")
+label_autocomplete = ctk.CTkLabel(tabs.tab("Console"), height=20, text="Enter a command", text_color="#5A5A5A", fg_color="#000000", font=("Monospace", 16))
+label_autocomplete.place(x=130, y=2.5)
+console_box = ctk.CTkScrollableFrame(tabs.tab("Console"), border_width=2, width=720, height=200, fg_color="#000000", border_color="#3C3C3C", scrollbar_button_color="#3C3C3C", scrollbar_button_hover_color="#3C3C3C")
 console_box.pack()
+entry_list.append({"element": to_do, "placeholder": [label_autocomplete]})
 
-label_add_action = ctk.CTkLabel(tabs.tab("Options"), text="Add a command").pack()
-entry_add_action = ctk.CTkEntry(tabs.tab("Options"), placeholder_text="Command's name").pack()
-combo_add_action = ctk.CTkComboBox(tabs.tab("Options"), values=["Web site", "Software"], state="readonly").pack()
-button_add_action = ctk.CTkButton(tabs.tab("Options"), text="Add").pack()
+label_add_action = ctk.CTkLabel(tabs.tab("Options"), text="Add a command", font=("Monospace", 18)).pack()
+name_add_action = ctk.CTkEntry(tabs.tab("Options"), width=200, textvariable=name_new_action, placeholder_text="Command's name", font=("Monospace", 18))
+name_add_action.pack()
+name_label = ctk.CTkLabel(tabs.tab("Options"), width=10, height=10, text="Command's name", font=("Monospace", 15))
+name_label.place(x=75, y=33)
+icon_add_action = ctk.CTkEntry(tabs.tab("Options"), width=200, textvariable=icon_new_action, placeholder_text="Icon's path", font=("Monospace", 18))
+icon_add_action.pack()
+icon_label = ctk.CTkLabel(tabs.tab("Options"), width=10, height=10, text="Icon's path", font=("Monospace", 15))
+icon_label.place(x=75, y=61)
+combo_add_action = ctk.CTkComboBox(tabs.tab("Options"), values=["Website", "Software"], state="readonly", font=("Monospace", 18), command=lambda value: lb_handle_type_action(value)).pack()
+adress_add_action = ctk.CTkEntry(tabs.tab("Options"), width=200, textvariable=adress_new_action, placeholder_text="Adress", font=("Monospace", 18))
+adress_add_action.pack_forget()
+adress_label = ctk.CTkLabel(tabs.tab("Options"), width=10, height=10, text="Adress", font=("Monospace", 15))
+adress_label.pack_forget()
+query_add_action = ctk.CTkEntry(tabs.tab("Options"), width=200, textvariable=query_new_action, placeholder_text="Search query", font=("Monospace", 18))
+query_add_action.pack_forget()
+query_label = ctk.CTkLabel(tabs.tab("Options"), width=10, height=10, text="Search query", font=("Monospace", 15))
+query_label.pack_forget()
+button_add_action = ctk.CTkButton(tabs.tab("Options"), text="Add", font=("Monospace", 18))
+button_add_action.pack()
+name_add_action.bind("<KeyRelease>", lambda key: lb_show_placeholder(name_new_action, "Command's name", name_label, 75, 33))
+icon_add_action.bind("<KeyRelease>", lambda key: lb_show_placeholder(icon_new_action, "Icon's path", icon_label, 75, 61))
+adress_add_action.bind("<KeyRelease>", lambda key: lb_show_placeholder(adress_new_action, "Adress", adress_label, 75, 117))
+query_add_action.bind("<KeyRelease>", lambda key: lb_show_placeholder(query_new_action, "Search query", query_label, 75, 145))
 
 speed_key = keyboard.Listener(on_press=lb_get_key)
 speed_key.start()
