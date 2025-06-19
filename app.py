07d577f6ee4bd4dd7d672b49ecbf0ff6def0702b
Louis Blonde <louis.blonde24@gmail.com>
2025-06-19 12:32:14 +0200

    update remove

--- app.py
+++ app.py
@@ -101,6 +101,7 @@ def lb_reset_colors(possible_search):
     label_autocomplete.configure(text="", fg_color=colors[color_index]["fg_color"])
     console_box.configure(fg_color=colors[color_index]["fg_color"], border_color=colors[color_index]["border_color"], scrollbar_button_color=colors[color_index]["border_color"], scrollbar_button_hover_color=colors[color_index]["hover"])
     tabs.configure(text_color=colors[color_index]["color_text_input"], segmented_button_fg_color=colors[color_index]["fg_color"], segmented_button_selected_color=colors[color_index]["border_color"], segmented_button_selected_hover_color=colors[color_index]["border_color"], segmented_button_unselected_color=colors[color_index]["fg_color"], segmented_button_unselected_hover_color=colors[color_index]["hover"])
+    back_grounder.configure(scrollbar_button_color=colors[color_index]["border_color"], scrollbar_button_hover_color=colors[color_index]["hover"])
     label_add_action.configure(fg_color=colors[color_index]["hover"], text_color=colors[color_index]["color_text_input"])
     name_add_action.configure(fg_color=colors[color_index]["fg_color"], text_color=colors[color_index]["color_text_input"], border_color=colors[color_index]["border_color"])
     name_label.configure(fg_color=colors[color_index]["fg_color"])
@@ -115,15 +116,18 @@ def lb_reset_colors(possible_search):
     path_add_action.configure(fg_color=colors[color_index]["fg_color"], text_color=colors[color_index]["color_text_input"], border_color=colors[color_index]["border_color"])
     path_label.configure(fg_color=colors[color_index]["fg_color"])
     button_add_action.configure(fg_color=colors[color_index]["fg_color"], text_color=colors[color_index]["color_text_input"], border_color=colors[color_index]["border_color"], hover_color=colors[color_index]["hover"])
-    back_grounder.configure(scrollbar_button_color=colors[color_index]["border_color"], scrollbar_button_hover_color=colors[color_index]["hover"])
+    label_remove_action.configure(fg_color=colors[color_index]["hover"], text_color=colors[color_index]["color_text_input"])
+    num_remove_action.configure(fg_color=colors[color_index]["fg_color"], border_color=colors[color_index]["border_color"])
+    num_label.configure(fg_color=colors[color_index]["fg_color"])
+    button_remove_action.configure(fg_color=colors[color_index]["fg_color"], text_color=colors[color_index]["color_text_input"], border_color=colors[color_index]["border_color"], hover_color=colors[color_index]["hover"])
 def lb_handle_type_action(value):
     combo_add_action.pack_forget()
     color_add_action.pack_forget()
     button_add_action.pack_forget()
     label_remove_action.pack_forget()
-    info_remove_action.pack_forget()
     num_remove_action.pack_forget()
     num_label.place(x=1000, y=1000)
+    info_remove_action.pack_forget()
     button_remove_action.pack_forget()
     if value == "Website":
         adress_add_action.pack(pady=5)
@@ -132,6 +136,7 @@ def lb_handle_type_action(value):
         query_label.place(x=269, y=171)
         path_add_action.pack_forget()
         path_label.place(x=1000, y=1000)
+        num_label.place(x=269, y=406)
     else:
         adress_add_action.pack_forget()
         query_add_action.pack_forget()
@@ -139,18 +144,20 @@ def lb_handle_type_action(value):
         adress_label.place(x=1000, y=1000)
         query_label.place(x=1000, y=1000)
         path_label.place(x=269, y=133)
+        num_label.place(x=269, y=369)
     combo_add_action.pack(pady=5)
     color_add_action.pack(pady=5)
     button_add_action.pack(pady=15)
-    label_remove_action.pack()
+    label_remove_action.pack(pady=10)
+    num_remove_action.pack(pady=20)
     info_remove_action.pack()
-    num_remove_action.pack()
-    if combo_add_action.get() == "Website":
-        num_label.place(x=269, y=393)
-    elif combo_add_action.get() == "Software":
-        num_label.place(x=269, y=355)
-    button_remove_action.pack()
+    button_remove_action.pack(pady=10)
 def lb_show_placeholder(value, text, label, x, y):
+    if text == 'Num':
+        if combo_add_action.get() == 'Software':
+            x, y = 269, 369
+        else:
+            x, y = 269, 406
     if value.get() == "":
         label.configure(text=text, font=("Monospace", 15))
         label.place(x=x, y=y)
@@ -183,10 +190,11 @@ def lb_add_new_action():
         lb_show_placeholder(path_new_action, "Path's file", path_label, 250, 133)
     with open("./assets/json/command_info.json", "w", encoding="utf-8") as data:
         data.write(lb_encrypt(f'{key_words}', encrypt_key))
+    info_remove_action.configure(text=lb_refresh_command())
     name_new_action.set("")
     icon_new_action.set("")
-    lb_show_placeholder(name_new_action, "Command's name", name_label, 250, 58)
-    lb_show_placeholder(icon_new_action, "Icon's path", icon_label, 250, 95)
+    lb_show_placeholder(name_new_action, "Command's name", name_label, 269, 58)
+    lb_show_placeholder(icon_new_action, "Icon's path", icon_label, 269, 95)
     combo_add_action.set("Software")
     color_add_action.set("Red")
     lb_handle_type_action("Software")
@@ -203,6 +211,26 @@ def lb_handle_window():
     else:
         root.deiconify()
         root.lift()
+def lb_refresh_command():
+    stringer = ''
+    print(key_words, 2)
+    for i in range(len(key_words)):
+        stringer += f'[{i}] - {key_words[i]['name']}\n'
+    return stringer
+def lb_remove_action():
+    if num_delete_action.get() != '':
+        try:
+            key_words.pop(int(num_delete_action.get()))
+            print(key_words, 1)
+            num_delete_action.set('')
+            lb_show_placeholder(num_delete_action, "Num", num_label, 0, 0)
+            info_remove_action.configure(text=lb_refresh_command())
+            with open("./assets/json/command_info.json", "w", encoding="utf-8") as data:
+                data.write(lb_encrypt(f'{key_words}', encrypt_key))
+        except:
+            PopUp(root, "Error", "200x100", "Invalid argument", color_index)
+    else:
+        PopUp(root, "Error", "200x100", "Missing argument", color_index)
 
 class App(ctk.CTk):
     def __init__(self, title, dimension):
@@ -269,6 +297,7 @@ icon_new_action = ctk.StringVar()
 adress_new_action = ctk.StringVar()
 query_new_action = ctk.StringVar()
 path_new_action = ctk.StringVar()
+num_delete_action = ctk.StringVar()
 
 to_do = ctk.CTkEntry(tabs.tab("Console"), width=500, textvariable=to_do_request, placeholder_text="Enter a command", fg_color="#000000", border_color="#3C3C3C", text_color="white", font=("Monospace", 18))
 to_do.pack(pady=6)
@@ -279,7 +308,7 @@ console_box = ctk.CTkScrollableFrame(tabs.tab("Console"), border_width=2, width=
 console_box.pack(pady=6)
 
 back_grounder = ctk.CTkScrollableFrame(tabs.tab('Options'), width=720, height=200, fg_color="#3C3C3C", scrollbar_button_color="#000000", scrollbar_button_hover_color="#1E1E1E")
-label_add_action = ctk.CTkLabel(back_grounder, fg_color="#1E1E1E", text_color="#ffffff", corner_radius=10, text="Add a command", font=("Monospace", 18))
+label_add_action = ctk.CTkLabel(back_grounder, fg_color="#1E1E1E", text_color="#ffffff", corner_radius=10, text="------------Add a command------------", font=("Monospace", 18))
 name_add_action = ctk.CTkEntry(back_grounder, fg_color="#000000", text_color="#ffffff", border_color="#3C3C3C", width=200, textvariable=name_new_action, placeholder_text="Command's name", font=("Monospace", 18))
 name_label = ctk.CTkLabel(back_grounder, fg_color="#000000", text_color="#5A5A5A", width=10, height=10, text="Command's name", font=("Monospace", 15))
 icon_add_action = ctk.CTkEntry(back_grounder, fg_color="#000000", text_color="#ffffff", border_color="#3C3C3C", width=200, textvariable=icon_new_action, placeholder_text="Icon's path", font=("Monospace", 18))
@@ -300,11 +329,12 @@ icon_add_action.bind("<KeyRelease>", lambda key: lb_show_placeholder(icon_new_ac
 adress_add_action.bind("<KeyRelease>", lambda key: lb_show_placeholder(adress_new_action, "Adress", adress_label, 269, 133))
 query_add_action.bind("<KeyRelease>", lambda key: lb_show_placeholder(query_new_action, "Search query", query_label, 269, 171))
 path_add_action.bind("<KeyRelease>", lambda key: lb_show_placeholder(path_new_action, "Path's file", path_label, 269, 133))
-label_remove_action = ctk.CTkLabel(back_grounder, fg_color="#1E1E1E", text_color="#ffffff", corner_radius=10, text="Remove a command", font=("Monospace", 18))
-info_remove_action = ctk.CTkLabel(back_grounder, fg_color="#000000", text_color="#ffffff")
-num_remove_action = ctk.CTkEntry(back_grounder, fg_color="#000000", text_color="#ffffff", border_color="#3C3C3C", width=200, textvariable=adress_new_action, placeholder_text="Adress", font=("Monospace", 18))
+label_remove_action = ctk.CTkLabel(back_grounder, fg_color="#1E1E1E", text_color="#ffffff", corner_radius=10, text="------------Remove a command------------", font=("Monospace", 18))
+num_remove_action = ctk.CTkEntry(back_grounder, textvariable=num_delete_action, fg_color="#000000", text_color="#ffffff", border_color="#3C3C3C", width=200, placeholder_text="Adress", font=("Monospace", 18))
 num_label = ctk.CTkLabel(back_grounder, fg_color="#000000", text_color="#5A5A5A", width=10, height=10, text="Num", font=("Monospace", 15))
-button_remove_action = ctk.CTkButton(back_grounder, border_width=2, fg_color="#000000", text_color="#ffffff", border_color="#3C3C3C", hover_color="#1E1E1E", text="Remove", font=("Monospace", 18), cursor="hand2", command=lb_add_new_action)
+info_remove_action = ctk.CTkLabel(back_grounder, text=lb_refresh_command(), text_color="#ffffff", font=('Monospcae', 18))
+button_remove_action = ctk.CTkButton(back_grounder, border_width=2, fg_color="#000000", text_color="#ffffff", border_color="#3C3C3C", hover_color="#1E1E1E", text="Remove", font=("Monospace", 18), cursor="hand2", command=lb_remove_action)
+num_remove_action.bind("<KeyRelease>", lambda key: lb_show_placeholder(num_delete_action, "Num", num_label, 0, 0))
 back_grounder.pack()
 label_add_action.pack(pady=10)
 name_add_action.pack(pady=5)
@@ -320,11 +350,11 @@ path_label.place(x=269, y=133)
 combo_add_action.pack(pady=5)
 color_add_action.pack(pady=5)
 button_add_action.pack(pady=15)
-label_remove_action.pack()
+label_remove_action.pack(pady=10)
+num_remove_action.pack(pady=20)
+num_label.place(x=269, y=369)
 info_remove_action.pack()
-num_remove_action.pack()
-num_label.place(x=269, y=355)
-button_remove_action.pack()
+button_remove_action.pack(pady=10)
 
 threading.Thread(target=lb_listen_key, daemon=True).start()
 
